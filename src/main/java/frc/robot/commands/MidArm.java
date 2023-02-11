b31a299791b27095237ebe38c3332bbf8e3f2dfc
dphagler <dphagler@gmail.com>
2023-02-11 14:32:06 -0500

    MidCommands

--- src/main/java/frc/robot/commands/MidArm.java
+++ src/main/java/frc/robot/commands/MidArm.java
@@ -5,16 +5,22 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Arm;
 
 public class MidArm extends CommandBase {
+  private final Arm m_Arm;
   /** Creates a new MidArm. */
-  public MidArm() {
+  public MidArm(Arm subsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
+    m_Arm = subsystem;
+    addRequirements(m_Arm);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_Arm.setPosition(-37);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
