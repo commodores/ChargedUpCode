4e72098c6e61c3d0e65c1250b4e13ecea3d17cd1
dphagler <dphagler@gmail.com>
2023-02-22 10:17:42 -0500

    updates

--- src/main/java/frc/robot/commands/HighElevator.java
+++ src/main/java/frc/robot/commands/HighElevator.java
@@ -23,7 +23,7 @@ public class HighElevator extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_Elevator.setPosition(197.6);
+    m_Elevator.setPosition(83);
   }
 
   // Called once the command ends or is interrupted.
--- src/main/java/frc/robot/commands/MidElevator.java
+++ src/main/java/frc/robot/commands/MidElevator.java
@@ -24,7 +24,7 @@ public class MidElevator extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_Elevator.setPosition(51.38);
+    m_Elevator.setPosition(27);
   }
 
   // Called once the command ends or is interrupted.
